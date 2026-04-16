e26864ed93609093dd1ede02cd50bc356aca6b83
invacuation <47639340+invacuation@users.noreply.github.com>
2026-04-16 10:17:26 +0100

    chore: change to hs3 to account for dependency rebuild

--- python/ray/_version.py
+++ python/ray/_version.py
@@ -1,6 +1,6 @@
 # Replaced with the current commit when building the wheels.
 commit = "{{RAY_COMMIT_SHA}}"
-version = "2.52.0+hs2"
+version = "2.52.0+hs3"
 
 if __name__ == "__main__":
     print("%s %s" % (version, commit))
